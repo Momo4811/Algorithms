798fbe7b9577960091008d6eb8a95ed90e858f4e
kwabenankyi <anthony.nkyi@outlook.com>
2024-03-01 19:34:47 +0000

    points sorted by simple linear operation
    using 1/tan(angle) to sort key rather than calculating actual angle

--- Graham/an_final_grahamscan.py
+++ Graham/an_final_grahamscan.py
@@ -1,4 +1,3 @@
-import math
 class Point:
     def __init__(self,x,y):
         self.x = x
@@ -15,31 +14,15 @@ class PointSymbolTable: #technically a binary tree
         self.right = None
     
     def valuesToList(self):#in order traversal to list
-        list = []
-        if self.left is not None:
-            list.extend(self.left.valuesToList())
-        list.append(self.value)
-        if self.right is not None:
-            list.extend(self.right.valuesToList())
-        return list
+        return (self.left.valuesToList() if self.left else []) + [self.value] + (self.right.valuesToList() if self.right else [])
     
     def get(self, key):#normal binary tree get 
-        if self.key == key:
-            return self.value
-        elif self.key < key:
-            if self.right is not None:
-                return self.right.key
-            return None
-        else:
-            if self.left is not None:
-                return self.left.key
-            return None
+        return self.value if self.key == key else (self.right.get(key) if self.key < key else self.left.get(key))
     
     def put(self, key, value, ref):#normal binary tree put
         if self.key == key:
             #always keep furthest point from ref
-            if value.calcDistance(ref) > self.value.calcDistance(ref):
-                self.value = value
+            self.value = value if value.calcDistance(ref) > self.value.calcDistance(ref) else self.value
         elif self.key < key:
             if self.right is not None:
                 self.right.put(key, value, ref)
@@ -54,25 +37,26 @@ class PointSymbolTable: #technically a binary tree
 def cross_product(p1,p2,p3):
     return (p2.x - p1.x)*(p3.y - p1.y) - (p3.x - p1.x)*(p2.y - p1.y)
 
-def getAngle(p1,p2):
-    return math.atan2(p2.y - p1.y, p2.x - p1.x)
+def sort_key(start,p1): #sort by -1/tan (angle)
+    try:
+        return - (p1.x - start.x) / (p1.y - start.y)
+    except ZeroDivisionError:
+        return float('inf') if p1.x - start.x < 0 else float('-inf')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 
 def akn_graham_scan(listOfPoints):
     stack=[]
-
     #find point with lowest y co-ordinate - if y is the same take lowest x
     point0 = listOfPoints[0]
     for current in listOfPoints[1:]:
         if (current.y < point0.y) or ((current.y == point0.y) and (current.x < point0.x)):
             point0 = current
-    
     #initialise symbol table with angle key and point value
-    symTable = PointSymbolTable(getAngle(point0,listOfPoints[0]),listOfPoints[0])
+    symTable = PointSymbolTable(sort_key(point0,listOfPoints[0]),listOfPoints[0])
 
     #calculate angles then sort by angle using symbol table
     for current in listOfPoints:
         if current != point0:
-            symTable.put(getAngle(point0,current),current,point0)
+            symTable.put(sort_key(point0,current),current,point0)
     
     sortedPoints = [point0] + symTable.valuesToList()
     #push first two points onto stack
@@ -80,7 +64,7 @@ def akn_graham_scan(listOfPoints):
     stack.append(sortedPoints[1])
     for i in range(2,len(sortedPoints)):
         while len(stack) > 1 and cross_product(stack[-2],stack[-1],sortedPoints[i]) <= 0:
-                #if < 0 then we are turning right so pop the last point from the stack
+                #if < 0 then we are turning right so pop the last point from the stack - anticlockwise search
                 stack.pop()
         stack.append(sortedPoints[i])
     return stack
